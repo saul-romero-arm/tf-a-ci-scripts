67596faf82eb7e5832656185951efa849e3f5d6d
Zelalem <zelalem.aweke@arm.com>
2020-07-01 11:59:37 -0500

    Add more build configurations for Coverity MISRA checks
    This patch adds more configs for Coverity MISRA checks.
    It covers more source files that are not covered right now.
    
    Unlike the MISRA checks we have now, these are nominated,
    i.e. they are only run on patches that touch specific source files.
    Therefore, this patch also updates the nomination rules file.
    I chose to create a new "tf-l2-coverity-misra-nominated" group to
    differentiate them from the ones that are always run.
    
    An ultimate goal is to move the configs (at least some of them) in the
    "tf-l2-coverity-misra" group to the new nominated group as we don't
    need to run all of them on every patch.
    
    Change-Id: I1939d075e587732c6e6239f509eeeb8e5afeac4e
    Signed-off-by: Zelalem <zelalem.aweke@arm.com>

--- script/trusted-firmware.nomination.py
+++ script/trusted-firmware.nomination.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2019, Arm Limited. All rights reserved.
+# Copyright (c) 2019-2020, Arm Limited. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -10,22 +10,74 @@
 # This file is EXECED from gen_nomination.py
 
 nomination_rules = {
-        # Run RDN1EDGE TF-A Tests for any platform changes
+        # Run RDN1EDGE TF-A Tests and MISRA checks for any platform changes
         "path:plat/arm/board/rdn1edge":
-            ["tf-l3-boot-tests-css/fvp-rdn1edge-tbb,fvp-rdn1edge-default:fvp-tftf-fip.tftf-rdn1edge"],
+            ["tf-l3-boot-tests-css/fvp-rdn1edge-tbb,fvp-rdn1edge-default:fvp-tftf-fip.tftf-rdn1edge",
+             "tf-l2-coverity-misra-nominated/fvp-rdn1edge-tbb:coverity-tf-misra.diff"],
 
-        # Run SGI575 boot test and TF-A Tests for any platform changes
-        "path:plat/arm/board/sgi575":
+        # Run SGI575 boot test, TF-A Tests and MISRA checks for any changes with "sgi" in the path
+        "pathre:sgi":
             ["tf-l3-boot-tests-css/fvp-sgi575-tbb:fvp-linux.sgi-fip.sgi-sgi575-debug",
-             "tf-l3-boot-tests-css/fvp-sgi575-tbb,fvp-sgi575-default:fvp-tftf-fip.tftf-sgi575"],
+             "tf-l3-boot-tests-css/fvp-sgi575-tbb,fvp-sgi575-default:fvp-tftf-fip.tftf-sgi575",
+             "tf-l2-coverity-misra-nominated/fvp-sgi575-tbb:coverity-tf-misra.diff"],
 
-        # Run SGM775 boot test for any platform changes
-        "path:plat/arm/board/sgm775":
-            ["tf-l3-boot-tests-css/fvp-sgm775-tbb:fvp-linux.sgm-dtb.sgm775-fip.sgm-sgm775-debug"],
+        # Run SGM775 boot test and MISRA checks for any changes with "sgm" in the path
+        "pathre:sgm":
+            ["tf-l3-boot-tests-css/fvp-sgm775-tbb:fvp-linux.sgm-dtb.sgm775-fip.sgm-sgm775-debug",
+             "tf-l2-coverity-misra-nominated/fvp-sgm775-tbb:coverity-tf-misra.diff"],
+
+        # Run Coverity MISRA checks for tc0 platform changes
+        "path:plat/arm/board/tc0":
+            ["tf-l2-coverity-misra-nominated/fvp-tc0-tbb:coverity-tf-misra.diff"],
+
+         # Run Coverity MISRA checks for n1sdp platform changes
+        "path:plat/arm/board/n1sdp":
+            ["tf-l2-coverity-misra-nominated/n1sdp-default:coverity-tf-misra.diff"],
+
+        # Run Coverity MISRA checks for arm_fpga platform changes
+        "path:plat/arm/board/arm_fpga":
+            ["tf-l2-coverity-misra-nominated/arm_fpga-default:coverity-tf-misra.diff"],
+
+        # Run Coverity MISRA checks for rde1edge platform changes
+        "path:plat/arm/board/rde1edge":
+            ["tf-l2-coverity-misra-nominated/fvp-rde1edge-tbb:coverity-tf-misra.diff"],
+
+        # Run Coverity MISRA checks for fvp_ve platform changes
+        "path:plat/arm/board/fvp_ve":
+            ["tf-l2-coverity-misra-nominated/fvp_ve-a7:coverity-tf-misra.diff"],
+
+        # Run Coverity MISRA checks for a5ds platform changes
+        "path:plat/arm/board/a5ds":
+            ["tf-l2-coverity-misra-nominated/a5ds:coverity-tf-misra.diff"],
+
+        # Run Coverity MISRA checks for corstone700 platform changes
+        "path:plat/arm/board/corstone700":
+            ["tf-l2-coverity-misra-nominated/corstone700-default:coverity-tf-misra.diff"],
+
+        # Run Coverity MISRA checks for SPM_MM changes
+        "pathre:spm_mm":
+            ["tf-l2-coverity-misra-nominated/fvp-spm-mm:coverity-tf-misra.diff"],
+
+         # Run Coverity MISRA checks for Debugfs changes
+         "pathre:debugfs":
+            ["tf-l2-coverity-misra-nominated/fvp-debugfs:coverity-tf-misra.diff"],
+
+        # Run Coverity MISRA checks for fconf changes
+        "pathre:fconf":
+            ["tf-l2-coverity-misra-nominated/fvp-aarch64-sdei-fconf:coverity-tf-misra.diff"],
+
+        # Run Coverity MISRA checks for pauth changes
+        "pathre:pauth":
+            ["tf-l2-coverity-misra-nominated/fvp-pauth-standard:coverity-tf-misra.diff"],
+
+        # Run Coverity MISRA checks for RAS extension changes
+        ("path:lib/extensions/ras", "pathre:fvp_ras"):
+            ["tf-l2-coverity-misra-nominated/fvp-ras-fault-inject:coverity-tf-misra.diff"],
 
         # Run SDEI boot test for SDEI, EHF, or RAS changes or mention
         ("pathre:sdei", "pathre:ehf", "pathre:ras", "has:SDEI_SUPPORT",
               "has:EL3_EXCEPTION_HANDLING"):
-            ["tftf-l2-fvp/fvp-aarch64-sdei,fvp-default:fvp-tftf-fip.tftf-aemv8a-debug"],
+            ["tftf-l2-fvp/fvp-aarch64-sdei,fvp-default:fvp-tftf-fip.tftf-aemv8a-debug",
+             "tf-l2-coverity-misra-nominated/fvp-aarch64-sdei:coverity-tf-misra.diff"],
 
         }
