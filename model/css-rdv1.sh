0a9d54737e6d088a73d4bb024ac74eccdd9513eb
Aditya Angadi <aditya.angadi@arm.com>
2021-01-13 11:28:34 +0530

    Update model version for rdv1 platform
    Update the model from 11.10.36 to 11.13.10 for RD-V1 platform.
    CMN-Rhodes has also been renamed to CMN-650 where applicable. This
    version of model also requires to use the latest SCP/MCP binaries after
    the renaming rddaniel to rdv1 in SCP-Firmware project.
    
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>
    Change-Id: Ice8462307a40800f961b0e91737bee2177ca1cdb

--- model/css-rdv1.sh
+++ model/css-rdv1.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2020, Arm Limited. All rights reserved.
+# Copyright (c) 2020-2021, Arm Limited. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
-# Tell FVP to use RNSAMs as internal in CMN-Rhodes.
-set_model_env "FASTSIM_CMN_INTERNAL_RNSAM" "1"
-
-set_model_path "$warehouse/SysGen/SubSystemModels/11.10/36/models/Linux64_GCC-6.4/FVP_RD_Daniel"
+set_model_path "$warehouse/SysGen/SubSystemModels/11.13/10/models/Linux64_GCC-6.4/FVP_RD_Daniel"
 
 cat <<EOF >"$model_param_file"
--C css.cmn_rhodes.force_on_from_start=1
+-C css.cmn_650.force_rnsam_internal=true
 -C css.mcp.ROMloader.fname=$mcp_rom_bin
 -C css.scp.ROMloader.fname=$scp_rom_bin
 --data css.scp.armcortexm7ct=$scp_ram_bin@$scp_ram_addr
