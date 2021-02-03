3dbabb63527607e3c9303264457daab267695d85
Madhukar Pappireddy <madhukar.pappireddy@arm.com>
2021-02-03 12:21:12 -0600

    Necessary changes to support TC0 platform with Theodul DSU and 8 cores
    Refer [1]. The new TC0 AEM Model needs modified parameters.
    Also updated the core count required by the expect script.
    New SCP binaries for the upgraded TC0 platform were uploaded
    to TF-A downloads static server.
    
    [1] https://review.trustedfirmware.org/c/TF-A/trusted-firmware-a/+/8182
    
    Signed-off-by: Madhukar Pappireddy <madhukar.pappireddy@arm.com>
    Change-Id: Ia921de086937550e6f6a5ab7e4b2bd23aec6e6ca

--- model/tc0.sh
+++ model/tc0.sh
@@ -11,8 +11,8 @@ cat <<EOF >"$model_param_file"
 ${bl1_bin+-C css.trustedBootROMloader.fname=$bl1_bin}
 ${scp_romfw_bin+-C css.scp.ROMloader.fname=$scp_romfw_bin}
 ${fip_bin+-C board.flashloader0.fname=$fip_bin}
-${initrd_bin+--data board.cpu=$initrd_bin@${initrd_addr:?}}
-${kernel_bin+--data board.cpu=$kernel_bin@${kernel_addr:?}}
+${initrd_bin+--data board.dram=$initrd_bin@${initrd_addr:?}}
+${kernel_bin+--data board.dram=$kernel_bin@${kernel_addr:?}}
 ${uart0_out+-C soc.pl011_uart0.out_file=$uart0_out}
 ${uart0_out+-C soc.pl011_uart0.unbuffered_output=1}
 ${uart1_out+-C soc.pl011_uart1.out_file=$uart1_out}
