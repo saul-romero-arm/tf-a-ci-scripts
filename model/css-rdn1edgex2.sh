8d0e20f42fe750f37c79783c381ac325d914869e
Leonardo Sandoval <leonardo.sandoval@linaro.org>
2021-03-24 19:12:59 -0600

    model/css-rdn1edgex2.sh: strip warehouse path for RD_N1_E1_cmn600_ccix.yml
    There is no need to set full path of RD_N1_E1_cmn600_ccix.yml file
    because it resides in the same place as model's binary, so setting a
    relative path is enough. This change also allows to pick the yaml file
    correctly by FVP rdn1edgex2 models launched at [1].
    
    [1] https://tf.validation.linaro.org/
    
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval@linaro.org>
    Change-Id: Ic13c2e9f64cfbdb47e298022fc4e4834419dae6b

--- model/css-rdn1edgex2.sh
+++ model/css-rdn1edgex2.sh
@@ -9,7 +9,7 @@ set_model_path "$warehouse/SysGen/SubSystemModels/11.10/36/models/Linux64_GCC-6.
 
 cat <<EOF >"$model_param_file"
 -C css0.cmn600.force_on_from_start=1
--C css0.cmn600.mesh_config_file=$warehouse/SysGen/SubSystemModels/11.10/36/models/Linux64_GCC-6.4/RD_N1_E1_cmn600_ccix.yml
+-C css0.cmn600.mesh_config_file=RD_N1_E1_cmn600_ccix.yml
 -C css0.mcp.ROMloader.fname=$mcp_rom_bin
 -C css0.scp.ROMloader.fname=$scp_rom_bin
 --data css0.scp.armcortexm7ct=$scp_ram_bin@$scp_ram_addr
@@ -21,7 +21,7 @@ cat <<EOF >"$model_param_file"
 -C soc0.pl011_uart1.unbuffered_output=1
 
 -C css1.cmn600.force_on_from_start=1
--C css1.cmn600.mesh_config_file=$warehouse/SysGen/SubSystemModels/11.10/36/models/Linux64_GCC-6.4/RD_N1_E1_cmn600_ccix.yml
+-C css1.cmn600.mesh_config_file=RD_N1_E1_cmn600_ccix.yml
 -C css1.mcp.ROMloader.fname=$mcp_rom_bin
 -C css1.scp.ROMloader.fname=$scp_rom_bin
 --data css1.scp.armcortexm7ct=$scp_ram_bin@$scp_ram_addr
