345873c7c0ebdafd8227d964865cd1a4bf69c172
Chris Kay <chris.kay@arm.com>
2021-04-27 13:24:51 +0100

    Permit MIT licenses when checking copyright
    Some source files in TF-A are distributed under the terms of the MIT
    license, as they are derived from other projects where the original
    files are licensed under the MIT license instead of the BSD-3-Clause
    license.
    
    Change-Id: I48d78acd4c3a5de717bfe5273971e0fe8fc514aa
    Signed-off-by: Chris Kay <chris.kay@arm.com>

--- script/static-checks/check-copyright.py
+++ script/static-checks/check-copyright.py
@@ -60,7 +60,7 @@ EOL = SPACING + '$'
 TIME_PERIOD = '[0-9]{4}(-[0-9]{4})?'
 
 # Any string with valid license ID, don't allow adding postfix
-LICENSE_ID = '.*(BSD-3-Clause|BSD-2-Clause-FreeBSD)([ ,.\);].*)?'
+LICENSE_ID = '.*(BSD-3-Clause|BSD-2-Clause-FreeBSD|MIT)([ ,.\);].*)?'
 
 # File must contain both lines to pass the check
 COPYRIGHT_LINE = LINE_START + 'Copyright' + '.*' + TIME_PERIOD + '.*' + EOL
