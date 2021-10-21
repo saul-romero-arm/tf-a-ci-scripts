8a598e987d741e1ff26fd203f2455334b789e166
Maksims Svecovs <maksims.svecovs@arm.com>
2021-10-21 14:50:46 +0100

    cleanup(scp): removing scp v2.8.0 mentions
    All the models are using scp v2.9.0 prebuilt version - removing
    reference to v2.8.0 version
    
    Signed-off-by: Maksims Svecovs <maksims.svecovs@arm.com>
    Change-Id: I00a556e555462642937e42565e365cf12ee03a86

--- utils.sh
+++ utils.sh
@@ -335,9 +335,8 @@ tf_downloads="${tf_downloads:-file:///downloads/}"
 tfa_downloads="${tfa_downloads:-file:///downloads/tf-a}"
 css_downloads="${css_downloads:-$tfa_downloads/css}"
 
-# SCP/MCP v2.8.0 release binaries
+# SCP/MCP v2.9.0 release binaries
 scp_mcp_downloads="${scp_mcp_downloads:-$tfa_downloads/css_scp_2.9.0}"
-css_downloads_280="${css_downloads_280:-$tfa_downloads/css_scp_2.8.0}"
 
 linaro_2001_release="${linaro_2001_release:-$tfa_downloads/linaro/20.01}"
 linaro_release="${linaro_release:-$linaro_2001_release}"
