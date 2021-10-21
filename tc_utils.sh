83a7a663fa9185dddcc933ceb377dd40cf22b482
Maksims Svecovs <maksims.svecovs@arm.com>
2021-10-21 14:45:13 +0100

    feat(scp): scp/mcp v2.9.0 update - total compute
    Updates total compute test suite to use v2.9.0 scp/mcp prebuilds
    
    Signed-off-by: Maksims Svecovs <maksims.svecovs@arm.com>
    Change-Id: I6dc9daeb5a18f2c44a97e54b9a4ddc3fd6d71b19

--- tc_utils.sh
+++ tc_utils.sh
@@ -7,10 +7,10 @@
 
 source "$ci_root/fvp_utils.sh"
 
-tc_prebuilts="${tc_prebuilts:-$tfa_downloads/total_compute/}"
+tc_prebuilts="${tc_prebuilts:-$tfa_downloads/total_compute}"
 
-# Pre-built SCP v2.8.0 release binaries
-scp_prebuilts="${scp_prebuilts:-$tfa_downloads/total_compute_scp_2.8.0/}"
+# Pre-built SCP binaries
+scp_prebuilts="${scp_prebuilts:-$scp_mcp_downloads}"
 
 fvp_kernels[fvp-tc-kernel]="$tc_prebuilts/Image"
 fvp_initrd_urls[fvp-tc-ramdisk]="$tc_prebuilts/uInitrd-busybox.0x88000000"
