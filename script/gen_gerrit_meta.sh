1ba9d2a2f3fd6eefa1c897ad52b1661bb1ea7710
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2021-11-17 12:45:00 +0300

    gen_gerrit_meta.sh: Use $GERRIT_CHANGE_NUMBER if available
    Different jobs seem to pass Gerrit info in different ways, so add an
    alternative code path using GERRIT_CHANGE_NUMBER/GERRIT_PATCHSET_NUMBER.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    Change-Id: Ie14504c5fec518d0e690afb3acddaff0185def75

--- script/gen_gerrit_meta.sh
+++ script/gen_gerrit_meta.sh
@@ -7,11 +7,14 @@
 # Generate Gerrit-related metadata for LAVA job definitions. This is
 # include file is supposed to be sourced from gen_*_yaml.sh files.
 
-if [ -n "${GERRIT_REFSPEC}" ]; then
-    gerrit_url=$(echo ${GERRIT_REFSPEC} |  awk -F/ '{print "https://review.trustedfirmware.org/c/" $4 "/" $5}')
 
+if [ -n "${GERRIT_CHANGE_NUMBER}" ]; then
+    gerrit_url="https://review.trustedfirmware.org/c/${GERRIT_CHANGE_NUMBER}/${GERRIT_PATCHSET_NUMBER}"
+elif [ -n "${GERRIT_REFSPEC}" ]; then
+    gerrit_url=$(echo ${GERRIT_REFSPEC} |  awk -F/ '{print "https://review.trustedfirmware.org/c/" $4 "/" $5}')
+fi
+
+if [ -n "${gerrit_url}" ]; then
     gerrit_meta="\
-  gerrit_project: ${GERRIT_PROJECT}
-  gerrit_branch: ${GERRIT_BRANCH}
   gerrit_url: ${gerrit_url}"
 fi
