105f38544263890a7e3ed0e64646a00e6db3d8c7
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2022-01-25 17:19:30 +0300

    fast-forward-master.sh: For now, disable the push of the updated repo
    For now, just run the script in "dry run" mode, so we can watch it and make
    sure it (and the whole process works well).
    
    For that, enable shell tracing (set -x).
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    Change-Id: I53772f0e20fa9ef5bd00e70fa95b1d58c93acdd8

--- script/scratch_scripts/fast-forward-master.sh
+++ script/scratch_scripts/fast-forward-master.sh
@@ -7,12 +7,18 @@
 
 # Fast forward master branch with integration
 
-set -e
+set -ex
 
 git clone ssh://$CI_BOT_USERNAME@review.trustedfirmware.org:29418/TF-A/trusted-firmware-a
 cd trusted-firmware-a
 git checkout master
 git merge --ff-only origin/integration
-git push origin master
+
+# On OpenCI, disable push for now, until we're confident enough we want to do
+# this automatically. See comments in https://linaro.atlassian.net/browse/TFC-223.
+if echo "$JENKINS_URL" | grep -q "arm.com"; then
+    git push origin master
+fi
+
 cd ..
 rm -rf trusted-firmware-a
