1008edcedf2e2b7f814356d262dd4f9e9826c16e
johpow01 <john.powell@arm.com>
2022-02-01 18:50:33 -0600

    feat(cpu): add Cortex X2 run config
    This patch adds a run config for Cortex X2 along with other supporting
    changes.
    
    Signed-off-by: John Powell <john.powell@arm.com>
    Change-Id: I5b0d0efbe899a50334d318ed447a5674031eff04

---
+++ model/cortex-x2.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2022, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+set_model_path "$warehouse/SysGen/Models/$model_version/$model_build/external/models/$model_flavour/FVP_Base_Cortex-X2x4"
+
+source "$ci_root/model/fvp_common.sh"
