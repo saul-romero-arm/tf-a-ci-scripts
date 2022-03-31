f95305b61cb1322226afb45e7a308765f05d720c
Maksims Svecovs <maksims.svecovs@arm.com>
2022-03-31 12:05:31 +0100

    fix(morello): fix path for OpenCI.
    downloads.trustedfirmware.org cannot cope with // in the path, fixing
    morello SCP/MCP prebuilts path.
    
    Signed-off-by: Maksims Svecovs <maksims.svecovs@arm.com>
    Change-Id: I291413e8871eb3047b6d25731552045f170bc0b0

--- morello_utils.sh
+++ morello_utils.sh
@@ -11,4 +11,4 @@ morello_prebuilts=${morello_prebuilts:="$tfa_downloads/morello"}
 
 # TODO: Restore this path once the SCP release v2.10 binaries are generated
 #scp_mcp_prebuilts=${scp_mcp_prebuilts:="$scp_mcp_downloads/morello/release"}
-scp_mcp_prebuilts=${scp_mcp_prebuilts:="$tfa_downloads/morello/"}
+scp_mcp_prebuilts=${scp_mcp_prebuilts:="$tfa_downloads/morello"}
