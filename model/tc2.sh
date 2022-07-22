396938a3a0625b682a9a3aa983aa8f0861338f89
Rupinderjit Singh <rupinderjit.singh@arm.com>
2022-07-22 21:19:41 +0100

    tc: Add support for TC2 platform CI tests
    This includes scp-boot-tests and tf-l3-boot-tests-css.
    
    Signed-off-by: Rupinderjit Singh <rupinderjit.singh@arm.com>
    Change-Id: I15ff11d1a890f8352f4469821fdceb803336768c

--- model/tc2.sh
+++ model/tc2.sh
@@ -8,8 +8,6 @@
 set_model_path "$warehouse/SysGen/SubSystemModels/$model_version/$model_build/models/$model_flavour/FVP_TC2"
 
 cat <<EOF >"$model_param_file"
-${bl1_bin+-C css.trustedBootROMloader.fname=$bl1_bin}
-${scp_rom_bin+-C css.scp.ROMloader.fname=$scp_rom_bin}
 ${fip_bin+-C board.flashloader0.fname=$fip_bin}
 ${initrd_bin+--data board.dram=$initrd_bin@${initrd_addr:?}}
 ${kernel_bin+--data board.dram=$kernel_bin@${kernel_addr:?}}
@@ -17,6 +15,10 @@ ${uart0_out+-C soc.pl011_uart0.out_file=$uart0_out}
 ${uart0_out+-C soc.pl011_uart0.unbuffered_output=1}
 ${uart1_out+-C soc.pl011_uart1.out_file=$uart1_out}
 ${uart1_out+-C soc.pl011_uart1.unbuffered_output=1}
+-C displayController=2
 ${rss_rom_file+--data css.rss.cpu=$rss_rom_file@${rss_rom_addr:?}}
 ${rss_flash_file+--data css.rss.cpu=$rss_flash_file@${rss_flash_addr:?}}
+${vmmaddrwidth+-C css.rss.VMADDRWIDTH=$vmmaddrwidth}
+${rvbaddr_lw+-C css.scp.c0_pik.rvbaraddr_lw=$rvbaddr_lw}
+${rvbaddr_up+-C css.scp.c0_pik.rvbaraddr_up=$rvbaddr_up}
 EOF
--- tc_utils.sh
+++ tc_utils.sh
@@ -18,8 +18,27 @@ fvp_initrd_urls[fvp-tc-ramdisk]="$tc_prebuilts/uInitrd-busybox.0x88000000"
 initrd_addr=0x8000000
 kernel_addr=0x80000
 scp_ram_addr=0x0bd80000
+
 rss_rom_addr=0x11000000
 rss_flash_addr=0x31000000
+vmmaddrwidth=23
+rvbaddr_lw=0x1000
+rvbaddr_up=0x0000
+
+if [ $plat_variant -eq 2 ]; then
+    if [ ! -f "$archive/rss_rom.bin" ]; then
+            url="$tc_prebuilts/tc$plat_variant/rss_rom.bin" saveas="rss_rom.bin" fetch_file
+	    archive_file "rss_rom.bin"
+    fi
+
+    if [ ! -f "$archive/rss_flash.bin" ]; then
+	    url="$tc_prebuilts/tc$plat_variant/rss_flash.bin" saveas="rss_flash.bin" fetch_file
+	    archive_file "rss_flash.bin"
+    fi
+fi
+
+rss_rom_file="$archive/rss_rom.bin"
+rss_flash_file="$archive/rss_flash.bin"
 
 # Hafnium build repo containing Secure hafnium binaries
 spm_secure_out_dir=secure_tc_clang
