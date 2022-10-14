ee82f6202b674309fab53aefa7fb90817939dcb0
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2022-10-14 16:45:23 +0100

    ci: update CSS models to their released version
    Updated CSS models to their released version.
    
    Change-Id: I0100e861277421fafe6badb8066fed496bc9fb97
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>

--- model/tc1.sh
+++ model/tc1.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
-set_model_path "$warehouse/SysGen/SubSystemModels/$model_version/$model_build/models/$model_flavour/FVP_TC1"
+set_model_path "$warehouse/SysGen/SubSystemModels/11.17/33/models/$model_flavour/FVP_TC1"
 
 cat <<EOF >"$model_param_file"
 ${bl1_bin+-C css.trustedBootROMloader.fname=$bl1_bin}
--- model/tc2.sh
+++ model/tc2.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
-set_model_path "$warehouse/SysGen/SubSystemModels/$model_version/$model_build/models/$model_flavour/FVP_TC2_RSS"
+set_model_path "$warehouse/SysGen/SubSystemModels/11.18/28/models/$model_flavour/FVP_TC2"
 
 cat <<EOF >"$model_param_file"
 ${fip_bin+-C board.flashloader0.fname=$fip_bin}
