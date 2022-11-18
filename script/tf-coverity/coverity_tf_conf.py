5433fcfe0c21fa7d7ae332fbc4ca924c3bce0675
Sona Mathew <SonaRebecca.Mathew@arm.com>
2022-11-18 15:22:02 -0600

    tf-coverity: Remove blank character in the file names.
    The file names in the coverity exclusion list had an extra blank character at the end
    "plat/st/stm32mp1/plat_def_uuid_config.c" and "drivers/io/io_dummy.c"
    
    Signed-off-by: Sona Mathew <SonaRebecca.Mathew@arm.com>
    Change-Id: Ie4cebcb777cff05c05882e645b9690fb48371b12

--- script/tf-coverity/coverity_tf_conf.py
+++ script/tf-coverity/coverity_tf_conf.py
@@ -106,10 +106,10 @@ exclude_paths = [
     ("plat/arm/css/sgi/sgi_ras.c", "Only used by deprecated SGI575 platform"),
 
     # Exclude the following files used for STM32MP host tools (fiptool and cert_create)
-    ("plat/st/stm32mp1/plat_def_uuid_config.c ", "Used to build STM32MP fiptool"),
+    ("plat/st/stm32mp1/plat_def_uuid_config.c", "Used to build STM32MP fiptool"),
     ("plat/st/stm32mp1/stm32mp1_tbb_cert.c", "Used to build STM32MP cert_create"),
 
     # Exclude the IO files
-    ("drivers/io/io_dummy.c ", "None of the upstream platforms using this file"),
+    ("drivers/io/io_dummy.c", "None of the upstream platforms using this file"),
 
 ]
