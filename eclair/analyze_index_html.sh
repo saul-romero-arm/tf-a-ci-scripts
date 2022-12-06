6848d1010ed5fa8e938360008844a044052ec2ff
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2022-12-06 20:37:34 +0300

    eclair/analyze_index_html.sh: Add more reports
    New reports:
    
    "Mandatory - violations" - the target is to have 0 here (and there's)
    "Mandatory - violations & cautions" - cautions may be worth a look
    "Report by issue strictness (Mandatory/Required/Advisory) (violations)"
    
    Also, reorder and update descriptions of existing reports.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    Change-Id: Id57dbea2656ab3a17386c12abf3623107c93b69e

--- eclair/analyze_index_html.sh
+++ eclair/analyze_index_html.sh
@@ -16,9 +16,30 @@ TF-A Config: ${TF_CONFIG}<br />
 CI Build: <a href="${BUILD_URL}">${BUILD_URL}</a>
 </p>
 
-<li><a href="ECLAIR/full_txt/">Full TXT report</a>
-<li><a href="ECLAIR/full_html/index.html">Full HTML report</a>
-<li><a href="ECLAIR/full_html/by_service.html#strictness/service/first_file&strictness">Report by issue strictness (Mandatory/Required/Advisory) (HTML).</a>
+Reports:
+<ul>
+<li><a href='ECLAIR/full_html/by_service.html#service/first_file&kind{"select":true,"selection":{"hiddenAreaKinds":[],"hiddenSubareaKinds":[],"show":true,"selector":{"enabled":true,"negated":false,"kind":1,"children":[{"enabled":true,"negated":false,"kind":0,"domain":"strictness","inputs":[{"enabled":true,"text":"mandatory"}]},{"enabled":true,"negated":false,"kind":0,"domain":"kind","inputs":[{"enabled":true,"text":"violation"}]}]}}}'>Mandatory rules - violations</a>
+<li><a href='ECLAIR/full_html/by_service.html#service/first_file&kind{"select":true,"selection":{"hiddenAreaKinds":[],"hiddenSubareaKinds":[],"show":true,"selector":{"enabled":true,"negated":false,"kind":1,"children":[{"enabled":true,"negated":false,"kind":0,"domain":"strictness","inputs":[{"enabled":true,"text":"mandatory"}]},{"enabled":true,"negated":false,"kind":0,"domain":"kind","inputs":[{"enabled":true,"text":"violation"},{"enabled":true,"text":"caution"}]}]}}}'>Mandatory rules - violations & cautions</a>
+<li><a href='ECLAIR/full_html/by_service.html#strictness/service/first_file&strictness{"select":true,"selection":{"hiddenAreaKinds":[],"hiddenSubareaKinds":[],"show":true,"selector":{"enabled":true,"negated":false,"kind":2,"children":[{"enabled":true,"negated":false,"kind":0,"domain":"kind","inputs":[{"enabled":true,"text":"violation"}]}]}}}'>Report by issue strictness (Mandatory/Required/Advisory) (violations)</a>
+<li><a href='ECLAIR/full_html/by_service.html#strictness/service/first_file&strictness'>Report by issue strictness (Mandatory/Required/Advisory) (all)</a>
+</ul>
+
+<ul>
+<li><a href="ECLAIR/full_html/index.html">Default ECLAIR report</a>
+<li><a href="ECLAIR/full_txt/">Default ECLAIR report (plain text)</a>
+</ul>
+
+<span style="font-size: 75%">
+<p>
+ECLAIR terminology cheatsheet:
+</p>
+<ul>
+<li>"violation" is formally proven issue
+<li>"caution" is <i>not</i> formally proven issue, may be a false positive
+<li>"information" is <i>not an issue</i> (from MISRA rules PoV), just FYI aka "know your codebase better"
+</ul>
+</span>
+
 </body>
 </html>
 EOF
